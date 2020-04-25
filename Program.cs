3bca1a8fa50ad867216063af834cad6ec12199b4
Avinash Sridharan <avsridha@microsoft.com>
2020-04-25 00:22:23 -0700

    Seeded the project with the basic PCA libraries.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Accord.Controls;
+using Accord.IO;
+using Accord.Math;
+using Accord.Statistics.Analysis;
+
+namespace pca_detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] matrix = {
+    new double[] { 1, 2 },
+    new double[] { 3, 4 },
+    new double[] { 5, 6 },
+};
+            PrincipalComponentAnalysis pca = new PrincipalComponentAnalysis(PrincipalComponentMethod.Center);
+
+            pca.Learn(matrix);
+
+        }
+    }
+}
